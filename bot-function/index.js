90d56d641b730f9b00c9c51207562692dd95df52
Sigrid <sigrid.huemer@posteo.at>
2021-06-24 15:00:25 +0200

    fix babel build

--- bot-function/index.js
+++ bot-function/index.js
@@ -3,10 +3,8 @@ require('dotenv').config()
 require('@babel/core').transform('code', {
   presets: ['@babel/preset-env'],
 })
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
 
-import * as functions from 'firebase-functions'
+import functions from 'firebase-functions'
 import { Telegraf } from 'telegraf'
 import { setupHelp, setupStart } from './commands/startAndHelp'
 import { setupCityBikes } from './commands/citybikes'
@@ -24,9 +22,24 @@ setupDrinkingFountains(bot)
 
 bot.launch()
 
+bot.telegram.setWebhook(
+  //https://${REGION!}-${PROJECT_ID!}.cloudfunctions.net/
+  `<change URL here>/bot-${process.env.BOT_TOKEN}`,
+)
+
+/*
 exports.bot = functions.https.onRequest((req, res) => {
   bot.handleUpdate(req.body, res)
 })
+*/
+
+export const botFunction = async (req, res) => {
+  try {
+    await bot.handleUpdate(req.body)
+  } finally {
+    res.status(200).end()
+  }
+}
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
