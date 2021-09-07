9e17a5228c63f6a07a7679ace62d35275458cf04
Sigrid <sigrid.huemer@posteo.at>
2021-09-07 14:07:36 +0200

    change redis settings

--- src/helpers/redis.js
+++ src/helpers/redis.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 import Redis from 'ioredis'
 
 export const redis = new Redis({
-  port: 13413,
-  host: 'redis-13413.c1.us-east1-2.gce.cloud.redislabs.com',
+  port: 15496,
+  host: 'redis-15496.c284.us-east1-2.gce.cloud.redislabs.com',
   password: process.env.REDIS_PASSWORT,
 })
